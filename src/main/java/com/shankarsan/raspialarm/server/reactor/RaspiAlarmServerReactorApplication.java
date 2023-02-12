892a0d63e4ab071cc824f2f59d3e5f7176226604
Shankarsan Ganai <shankershawn@gmail.com>
2023-02-12 23:32:27 +0530

    Initial commit

---
+++ src/main/java/com/shankarsan/raspialarm/server/reactor/RaspiAlarmServerReactorApplication.java
@@ -0,0 +1,13 @@
+package com.shankarsan.raspialarm.server.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspiAlarmServerReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspiAlarmServerReactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankarsan/raspi/alarm/server/reactor/RaspiAlarmServerReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankarsan.raspi.alarm.server.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspiAlarmServerReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
